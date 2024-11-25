cfc07052245cb7bcf53ab33ef130d1c4be526e10
jst-MW <unknown>
2024-11-25 08:36:15 -0600

    add part of f and g

--- src/main/java/SnakesLaddersBoardGame/SnakesLaddersBoard.java
+++ src/main/java/SnakesLaddersBoardGame/SnakesLaddersBoard.java
@@ -1,6 +1,7 @@
 package SnakesLaddersBoardGame;
 
 import java.lang.StringBuilder;
+import java.util.Random;
 
 public class SnakesLaddersBoard {
 
@@ -13,8 +14,16 @@ public class SnakesLaddersBoard {
             expand = 0;
         }
 
-        public String toString() {
-            return path + ":" + expand;
+        public int getPath() {
+            return path;
+        }
+
+        public int getExpand() {
+            return expand;
+        }
+
+        public void setPath(int newPath) {
+            path = newPath;
         }
     }
 
@@ -24,6 +33,7 @@ public class SnakesLaddersBoard {
     public SnakesLaddersBoard(int number) {
 
         addSpaces(number);
+        modifyBoard();
         playerPosition = 0;
 
     }
@@ -37,17 +47,88 @@ public class SnakesLaddersBoard {
         board = newBoard;
     }
 
+    /**
+     * make some spaces ladders and some snakes
+     * defualt is %10 fo the space number
+     *
+     */
+    private void modifyBoard() {
+        int howMany = board.size() / 10;
+
+        changeSpacesToSnakes(howMany);
+        changeSpacesToLadders(howMany);
+
+    }
+
+    private void changeSpacesToLadders(int howMany) {
+
+        int start = 1;
+        int end = board.size()-1;
+        Random rand = new Random();
+
+        for (int i = 0; i < howMany; i++) {
+            int from = rand.nextInt(end) + start;
+            int to = rand.nextInt(end) + start + from;
+            while (from < start || to > end) {
+                // if (hasRole(board.get(from)) && hasRole(board.get(to))) {
+                // continue;
+                // }
+                //ToDo: must prevent reassigning a role to an space that already has a role assigned to it!
+
+                from = rand.nextInt(end) + start;
+                to = rand.nextInt(end) + start + from;
+            }
+            System.out.println(from + "->" + to);
+            board.get(to).setPath(from);
+        }
+
+    }
+
+    private void changeSpacesToSnakes(int howMany) {
+
+        int start = 1;
+        int end = board.size()-1;
+        Random rand = new Random();
+
+        for (int i = 0; i < howMany; i++) {
+            int from = rand.nextInt(end) + start;
+            int to = rand.nextInt(end) + start - from;
+            while (from < start || to > end || to < from) {
+                from = rand.nextInt(end) + start;
+                to = rand.nextInt(end) + start - from;
+            }
+            System.out.println(to + "->" + from);
+            board.get(to).setPath(from);
+        }
+
+    }
+
+    private boolean hasRole(Space space) {
+        return space.getPath() == -1;
+    }
+
     private void addSpaces(int number, int index) {
     }
 
     public String toString() {
-        StringBuilder toPrint = new StringBuilder();
+        StringBuilder toPrint = new StringBuilder("@ ");
 
-        for (Space space : board) {
-            toPrint.append(space.toString());
+        for (int i = 1; i < board.size(); i++) {
+            toPrint.append(i);
+            if (board.get(i).getExpand() > 0) {
+                toPrint.append("+");
+            } else if (board.get(i).getExpand() < 0) {
+                toPrint.append("-");
+            }
+            if (board.get(i).getPath() != -1) {
+                toPrint.append("->");
+                toPrint.append(board.get(i).getPath());
+                toPrint.append(" ");
+            } else {
+                toPrint.append(" ");
+            }
         }
         return toPrint.toString();
     }
-
 
 }
