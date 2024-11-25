78196409ad6d243718f41cef6e3cf85e58ef5c6e
jst-MW <unknown>
2024-11-24 21:18:38 -0600

    add part b: write nested class space in SnakeLaddersBoard.java

--- src/main/java/SnakesLaddersBoardGame/SnamkesLaddersBoard.java
+++ src/main/java/SnakesLaddersBoardGame/SnamkesLaddersBoard.java
@@ -1,4 +1,16 @@
 package SnakesLaddersBoardGame;
 
 public class SnamkesLaddersBoard {
+
+    private class Space {
+        private int path;
+        private int expand;
+
+        public Space() {
+            path = -1;
+            expand = 0;
+        }
+
+    }
+
 }
