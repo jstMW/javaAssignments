bc6226f9efdf0f07800775d182e19a4acbd2a3ce
jst-MW <unknown>
2024-12-17 22:58:30 -0600

    update: farecode and flyerStatuscode will assign using randomValue method of respective enums

--- src/main/java/Ass3/q2/Passenger.java
+++ src/main/java/Ass3/q2/Passenger.java
@@ -6,8 +6,25 @@ public class Passenger {
 
     public Passenger(String passportNo, String fareCode, String flyerStatusCode, Date registerationDate) {
         this.passportNo = passportNo;
-        this.fareCode = fareCode;
-        this.flyerStatusCode = flyerStatusCode;
+        this.fareCode = Fare.randomValue();
+        this.flyerStatusCode = FlyerStatus.randomValue();
         this.registerationDate = new Date();
     }
+
+    public Passenger PassengerComparator(Passenger a, Passenger b) {
+        int compareFare = compare(a.getFare(), b.getFare());
+        if ( compareFare != 0) {
+            return compareFare > 0 ? a : b;
+        }
+        int compareFlyerStatus = compare(a.getFlyerStatus(), b.getFlyerStatus());
+        if (compareFlyerStatus != 0) {
+            return compareFlyerStatus > 0 ? a : b;
+        }
+
+        //nothing was mentioned for a case that two passnger could have the same Date so, no need to check if != 0
+        int compareDate = comaper(a.getDate(), b.getDate()); 
+        return compareDate > 0 ? a : b;
+
+}
+}
 }
