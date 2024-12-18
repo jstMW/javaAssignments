6b630be04cec1de8df549f937f9951dbf2bee955
jst-MW <unknown>
2024-12-17 22:34:50 -0600

    add: randomValue method to Fare

--- src/main/java/Ass3/q2/Fare.java
+++ src/main/java/Ass3/q2/Fare.java
@@ -1,3 +1,12 @@
+import java.util.Random;
 public Enum Fare {
     FULL, DISC, BUDDY
+
+    public Fare randomValue() {
+        Random rand = new Random();
+        int ordinal = rand.nextInt()%3;
+        for (Fare f: Fare.values()) {
+            if (f.ordinal() == ordinal) { return f; }
+    }
+}
 }
